289a1133b728f6b38be8b074b883157c5867f136
Charcoalie06 <charliekock06@gmail.com>
2025-10-02 15:01:28 +0200

    Topmost file

--- topmost.py
+++ topmost.py
@@ -1,1 +1,18 @@
+import sys
+from counting_and_printing_test import printTopMost
+from counting_and_printing_test import countWords
+from wordfreq import tokenize
 
+inp_file = open(sys.argv[1], encoding="utf-8")
+art_file = open(sys.argv[2], encoding="utf-8")
+printAmount = sys.argv[3]
+
+def main(inp_file, art_file, printAmount):
+    tokenized_words = tokenize(art_file)
+    art_file.close()
+    count_words = countWords(tokenized_words, inp_file)
+    inp_file.close()
+    printTopMost(count_words, printAmount)
+
+
+main(inp_file, art_file, printAmount)
