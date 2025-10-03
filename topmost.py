3d3628b770bd914a153fa61867e58c51e5a1d8ca
user1234662 <jontefilipsson@gmail.com>
2025-10-03 15:53:38 +0200

    new

--- topmost.py
+++ topmost.py
@@ -1,11 +1,17 @@
 from lab import wordfreq
+import urllib
 import sys
 
 def main(inp_file, art_file, printAmount):
-    #Call everything
-    
-    with open(sys.argv[2], encoding="utf-8") as art_file:
-        tokenized_words = wordfreq.tokenize(art_file)
+    #Call everything[2]:
+
+    if "http//" in sys.argv[2] or "https//" in sys.argv[2]:
+        response = urllib.request.urlopen(sys.argv[2])
+        lines = response.read().decode("utf8").splitlines()
+        tokenized_words = wordfreq.tokenize(lines)
+    else:
+        with open(sys.argv[2], encoding="utf-8") as art_file:
+            tokenized_words = wordfreq.tokenize(art_file)
 
     with open(sys.argv[1], encoding="utf-8") as inp_file:
         tokenized_stopWords = wordfreq.tokenize(inp_file)
